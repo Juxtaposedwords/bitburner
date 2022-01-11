60e0e71885f2216977d48bfac3e0d5396a17f93d
uhhhclem <rrossney@gmail.com>
2022-01-11 05:06:10 -0500

    Why do scripts run on remote servers require the import path to begin with a period?

--- out/automation/util/grow.js
+++ out/automation/util/grow.js
@@ -1,6 +1,6 @@
 /** @param {import("../../..").NS } ns */
 
-import { log } from  "/automation/lib/log.js"
+import { log } from  "./automation/lib/log.js"
 
 // grow continuously grows the target server.
 export async function main(ns) {
