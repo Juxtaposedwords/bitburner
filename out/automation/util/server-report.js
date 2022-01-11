6ea7a5e3ed9c86f1923dc19835fbafacc4afe565
juxtaposedwords <juxtaposedwords@gmail.com>
2022-01-10 17:13:30 -0800

    Giving server-report flags

--- out/automation/util/server-report.js
+++ out/automation/util/server-report.js
@@ -3,13 +3,19 @@
 import { servers } from "/automation/lib/scan.js"
 
 export async function main(ns) {
-	const which = ns.args[0]
-	let by = ns.args[1]
+	const data = ns.flags([
+		["ports", "open"], // whether to use servers which have open prots or not
+		["sort_by", "moneyAvailable"], // what to sort entries by
+		["pretty", false], // determines whether ot use pretty format or not
+		["top", 0], // print only the top X entries. by default all are printed
+	]);
+	const which = data["ports"]
+	let by = data["sort_by"]
 	if (which != "open" && which != "closed") {
 		ns.tprint("WARN:  Usage: run server-report.js open|closed ?sort-by")
 		return
 	}
-	if (by == undefined) { by = "moneyAvailable"}
+	if (by == undefined) { by = "moneyAvailable" }
 	const fields = [
 		"hostname",
 		"hackingLevel",
@@ -19,7 +25,7 @@ export async function main(ns) {
 	]
 	let field = undefined;
 	for (let i = 0; i < fields.length; i++) {
-		if (by == fields[i]) { 
+		if (by == fields[i]) {
 			field = i;
 			break;
 		}
@@ -34,11 +40,12 @@ export async function main(ns) {
 		if (which == "open" && !hasRoot) { continue }
 		if (which == "closed" && hasRoot) { continue }
 		result.push([
-			s, 
+			s,
 			ns.getServerRequiredHackingLevel(s),
 			Math.floor(ns.getServerSecurityLevel(s)),
-			Math.floor(ns.getServerMoneyAvailable(s)), 
+			Math.floor(ns.getServerMoneyAvailable(s)),
 			ns.getServerMaxMoney(s),
+			ns.getServerMinSecurityLevel(s),
 		])
 	}
 
@@ -49,19 +56,24 @@ export async function main(ns) {
 		r[3] = ns.nFormat(r[3], '0.0a');
 		r[4] = ns.nFormat(r[4], '0.0a');
 	}
-	ns.tprint("\n" + result.join('\n'));
+	if (!data['pretty']) {
+		ns.tprint("\n" + result.join('\n'));
+		return
+	} 
+	result.shift()
 
-	if (ns.args[2] == undefined ){
+	if (!data['pretty'] && data['top'] > 0) {
+		ns.tprintf("ERROR: invalid usage. Cannot use top without specifying pretty")
 		return
 	}
-	result.shift()
-	
-	for (let i = 1; i < result.length; i++) {
-		ns.tprintf("%s\n",result[i][0])
-		ns.tprintf("  Hack Level      : %d\n", result[i][1])
+
+	var length = (data['top']>0 && result.length>data['top']) ? data['top'] : result.length; 
+	for (let i = 1; i < length; i++) {
+		ns.tprintf("%s\n", result[i][0])
+		ns.tprintf("  Hack Level      : %d/%d\n", result[i][1],result[i][5])
 		ns.tprintf("  Security Level  : %d\n", result[i][2])
 		ns.tprintf("  Money Available : $%s\n", result[i][3])
-		ns.tprintf("  Max Money       : $%s\n",result[i][4])
+		ns.tprintf("  Max Money       : $%s\n", result[i][4])
 	}
 
 
