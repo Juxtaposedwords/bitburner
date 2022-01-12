e85d99c0a0b622734104022baa66d80519602861
juxtaposedwords <juxtaposedwords@gmail.com>
2022-01-11 19:39:09 -0800

    Checkingin some minor changes to the logger function.

--- out/automation/util/json-logger.js
+++ out/automation/util/json-logger.js
@@ -1,10 +1,10 @@
+import { servers } from "../lib/scan";
 
 
 /**
  * TODO(juxtaposedwords): find a way to make the need for 'await'  not necesasry when calling
  * TODO(juxtaposedwords): Add logging levels
- * TODO(juxtaposedwords): Add bg function which copies over file after write to home machine for central logging.
- * TODO(juxtaposedwords): Add log truncation at 1000 lines.
+ * TODO(juxtaposedwords): Add log file line truaction.
  * Json-logger creates a listener which listens to port 2 and writes everything received to a file asssociated with the logging services' name.
  *  Usage example:
  *      export async function main(ns) {
@@ -15,7 +15,11 @@
  * 
  *  @param {import("../../..").NS } ns */
 export async function main(ns) {
-    const port = ns.getPortHandle(2);
+    const data = ns.flags([
+		["port", 2], // Which port the listener should listen to.
+        ["log-dest", "home"], // Which port the listener should listen to.
+	]);
+    const port = ns.getPortHandle(data["port"]);
     await ns.disableLog('ALL')
     while (true) {
         if (port.empty()) {
@@ -26,20 +30,24 @@ export async function main(ns) {
 
         let entry = JSON.parse(v);
 
+        // Write the raw json log.
         const rawFile = `/logs/raw/${ns.getHostname()}.txt`;
-        if (ns.fileExists(rawFile)) {
-            await ns.write(rawFile, v+"\n", "a")
-        } else {
-            await ns.write(rawFile, v+"\n", "w")
-        }
+        // we can't use a variable for the "a" in this context, as the letter a is not the flag append.
+        update(ns, rawFile, v + "\n", data["log-dest"])
+
+        // Write out the structured log,
         const outputFile = `/logs/${ns.getHostname()}/${entry.program}.txt`
         let line = `${entry.datetime} ${entry.program} : ${entry.message}\n`
-
-        if (ns.fileExists(outputFile)) {
-            await ns.write(outputFile, line, "a")
-        } else {
-            await ns.write(outputFile, line, "w")
-
-        }
+        update(ns, outputFile, line, data["log-dest"])
     }
+}
+
+async function update(ns, fileName, line, destination) {
+    if (ns.fileExists(fileName)) {
+        await ns.write(fileName, line, "a")
+    } else {
+        await ns.write(fileName, line, "w")
+    }
+    ns.scp(fileName, ns.getHostname(), destination)
+
 }
