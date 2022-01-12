1bf80a323bf42f1e8595d2541b62590c4da37e6d
juxtaposedwords <juxtaposedwords@gmail.com>
2022-01-12 10:45:44 -0800

    Updating bg to use min security level rather than a derivative.

--- out/automation/util/bg.js
+++ out/automation/util/bg.js
@@ -9,7 +9,7 @@ export async function main(ns) {
     var moneyThresh = ns.getServerMaxMoney(target) ;
     var securityThresh = ns.getServerMinSecurityLevel(target) + 5;
     while(true) {
-        if (ns.getServerSecurityLevel(target) > securityThresh) {
+        if (ns.getServerSecurityLevel(target) > ns.getServerMinSecurityLevel(target)) {
             await ns.weaken(target);
         } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
             await ns.grow(target);
