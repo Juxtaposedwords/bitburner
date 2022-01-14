13edb0a612b86025e1ebdcae104a1e50ce3c376a
uhhhclem <rrossney@gmail.com>
2022-01-13 22:17:12 -0500

    script-income now works, and reports all servers

--- out/automation/commands/script-income.js
+++ out/automation/commands/script-income.js
@@ -1,12 +1,27 @@
 /** @param {import("../../..").NS } ns */
 
-// Report the income for each hack script running on home.
+import { scan } from "./automation/lib/scan.js"
+
+// Report the income for all scripts on all servers
 export async function main(ns) {
     const result = []
-    const script = "/automation/util/hack.js"
-    ns.ps("home")
-        .filter((s) => s.filename==script)
-        .forEach(s => result.push([s.args, ns.getScriptIncome(s.filename, "home", s.args)])
-    );
-    ns.tprint(result.join("\n"));
+    for (const server of scan(ns)) {
+        for (const s of ns.ps(server)) {
+            const inc = ns.getScriptIncome(s.filename, server, ...s.args)
+            if (inc > 0) {
+                result.push([server, s.filename + " " + s.args, inc])
+            }
+        }
+    }
+    result.sort(function(a, b) {
+        // ascending order by server
+        if (a[0] > b[0]) { return 1} 
+        else if (a[0] < b[0]) { return -1 }
+        // descending order by amount
+        else if (a[1] < b[1]) { return 1 }
+        else if (a[1] > b[1]) { return -1 }
+        else return 0;
+    })
+
+    ns.tprint("\n" + result.map(s => [s[0], s[1], ns.nFormat(s[2], '0.0a')].join(",")).join("\n"));
 }
