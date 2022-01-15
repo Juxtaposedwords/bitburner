94a767efc37ce3814027d57a330230cce2cdea53
uhhhclem <rrossney@gmail.com>
2022-01-15 18:27:40 -0500

    script-income includes purchased servers

--- out/automation/commands/script-income.js
+++ out/automation/commands/script-income.js
@@ -1,12 +1,12 @@
 // @ts-ignore
-import { scan } from "/automation/lib/scan.js"
+import { allServers } from "/automation/lib/scan.js"
 import { pad } from "./automation/lib/pad.js"
 
 /**  Report the income for all scripts on all servers
 * @param {import("../../..").NS } ns */
 export async function main(ns) {
     const result = []
-    for (const server of scan(ns)) {
+    for (const server of allServers(ns)) {
         for (const s of ns.ps(server)) {
             const inc = ns.getScriptIncome(s.filename, server, ...s.args)
             if (inc > 0) {
