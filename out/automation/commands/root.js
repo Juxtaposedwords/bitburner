90c8eff82cb2c60d50ac4f5e56c4fe2b2a2ca5cc
juxtaposedwords <juxtaposedwords@gmail.com>
2022-01-19 09:20:54 -0800

    Updating root command and grow-hack-weak

--- out/automation/commands/root.js
+++ out/automation/commands/root.js
@@ -5,7 +5,10 @@ import { scan } from "/automation/lib/scan.js"
 
 /** @param {import("../../..").NS } ns */
 export async function main(ns) {
-    for (const server of scan(ns)){
+    for (const server of scan(ns).filter(server =>{
+        const serverStats = ns.getServer(server)
+        return (!serverStats.hasAdminRights && ns.getHackingLevel() >= serverStats.hackDifficulty) 
+    })){
         root(ns, server,true)
     }
 }
