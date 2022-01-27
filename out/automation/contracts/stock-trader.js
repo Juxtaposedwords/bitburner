038ac9b2493934bc347eab01cb8602ae6ceb4199
juxtaposedwords <juxtaposedwords@gmail.com>
2022-01-27 15:38:27 -0800

    Adding stock trader  3 and 4

--- out/automation/contracts/stock-trader.js
+++ out/automation/contracts/stock-trader.js
@@ -47,6 +47,72 @@ export function unlimitedTransactions(ns, input) {
 }
 
 
+/** 
+ * You are given an array of numbers representing stock prices, where the
+ * i-th element represents the stock price on day i.
+ * 
+ * Determine the maximum possible profit you can earn using at most two
+ * transactions. A transaction is defined as buying and then selling one share
+ * of the stock. Note that you cannot engage in multiple transactions at once.
+ * In other words, you must sell the stock before you buy it again. If no profit
+ * can be made, then the answer should be 0.
+ * @param {import("../../..").NS } ns */
+export function twoTransactions(ns, input) {
+    if (input.length == 0) {
+        return 0;
+    }
+
+    var windows = findProfitWindows(input);
+    return maxProfit(windows, 2);
+
+}
+
+
+/**
+ *   You are given an array with two elements. The first element is an integer k.
+ * The second element is an array of numbers representing stock prices, where the
+ * i-th element represents the stock price on day i.
+ * 
+ *   Determine the maximum possible profit you can earn using at most k transactions.
+ * A transaction is defined as buying and then selling one share of the stock.
+ * Note that you cannot engage in multiple transactions at once. In other words,
+ * you must sell the stock before you can buy it. If no profit can be made, then
+ * the answer should be 0.
+ * @param {import("../../..").NS } ns */
+ export function kTransactions(ns, input) {
+    if (input[1].length == 0) {
+        return 0;
+    }
+
+    var windows = findProfitWindows(input[1]);
+    return maxProfit(windows, input[0]);
+
+}
+
+/**
+ *  fredProfit didn't have the same ring.*/
+function maxProfit(windows, k) {
+    if (k == 0 || windows.length == 0) {
+        return 0;
+    }
+
+    var c0 = windows[0];
+    if (windows.length == 1) {
+        return c0[1] - c0[0];
+    }
+
+    var profit = maxProfit(windows.slice(1), k);
+
+    for (var i = 0; i < windows.length; i++) {
+        var p = windows[1] - windows[0][0] + maxProfit(windows.slice(i + 1), k - 1);
+        if (p > profit) {
+            profit = p;
+        }
+    }
+    return profit;
+}
+
+
 function findProfitWindows(input) {
     var start = input[0];
     var end = start;
