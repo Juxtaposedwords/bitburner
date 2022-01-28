0044553bcb311a5f149ecf5cc1e9db85e2330ee3
juxtaposedwords <juxtaposedwords@gmail.com>
2022-01-27 16:34:26 -0800

    ADding subarray with max sum

--- out/automation/contracts/sum.js
+++ out/automation/contracts/sum.js
@@ -2,10 +2,17 @@
  * Given a number, how many different ways can that number be written as
  * a sum of at least two positive integers?
  * @param {import("../../..").NS } ns */
-function totalWayToSum(ns, input) {
+export function totalWayToSum(ns, input) {
     let cache = {};
     let n = input;
     return waysToSum(n, n, cache) - 1;
+}
+/**
+ * Given an array of integers, find the contiguous subarray (containing
+ * at least one number) which has the largest sum and return that sum.
+ * @param {import("../../..").NS } ns */
+export function subarrayWithMaxSum(ns, input) {
+    return findMaxSubArraySum(input);
 }
 
 function waysToSum(limit, n, cache) {
@@ -32,4 +39,22 @@ function waysToSum(limit, n, cache) {
         cache[n] = {};
     }
     cache[n][limit] = s; return s;
+}
+
+function findMaxSubArraySum(arr) {
+    if (arr.length == 0) {
+        return 0;
+    }
+    if (arr.length == 1) {
+        return arr[0];
+    }
+    var sum = findMaxSubArraySum(arr.slice(1));
+    var s = 0;
+    for (var i = 0; i < arr.length; i++) {
+        s += arr;
+        if (s > sum) {
+            sum = s;
+        }
+    }
+    return sum;
 }
