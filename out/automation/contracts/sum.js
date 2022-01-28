d91f9225ab5bcae6fd40ae786acfd97ba8452bab
juxtaposedwords <juxtaposedwords@gmail.com>
2022-01-27 23:41:08 -0800

    Fixed edge case and removed unused variable

--- out/automation/contracts/sum.js
+++ out/automation/contracts/sum.js
@@ -12,9 +12,8 @@ export function totalWayToSum(ns, input) {
  * at least one number) which has the largest sum and return that sum.
  * @param {import("../../..").NS } ns */
 export function subarrayWithMaxSum(ns, input) {
-    let arr = input
     let maxSum = input[0] +input[1] 
-    for (let i = 0; i <input.length;i++){
+    for (let i = 0; i <input.length-1;i++){
         for (let j = i+1; j < input.length; j++){
             let sum = input.slice(i,j).reduce((a, b) => a + b);
             if (sum > maxSum) {
