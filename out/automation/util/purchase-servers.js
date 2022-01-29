4d7a5fe14e86ce85d8dda05a7bf060aebf90d56d
juxtaposedwords <juxtaposedwords@gmail.com>
2022-01-29 14:06:48 -0800

    adding purchase servesr workign script.

--- out/automation/util/purchase-servers.js
+++ out/automation/util/purchase-servers.js
@@ -4,35 +4,30 @@
  *  @param {import("../../..").NS } ns */
 export async function main(ns) {
 	// When you run this script, specify the target on the command line.
-	const ram = Number(ns.args[0]);
-	const target = String(ns.args[1]);
-	const minMoneyThreshold = (ns.args[2] == undefined) ? 0 : ns.args[2];
+	let ramInput =Number(ns.args[0])
+	const ram = Math.pow(2, Number(ramInput));
+	const minMoneyThreshold = (ns.args[1] == undefined) ? 0 : Number(ns.args[2]);
 
-	if (target == undefined || ram == undefined) {
+	if (ns.args[0] == undefined) {
 		ns.tprint("WARN: Usage: run purchaseServers.js <ram> <target>")
 		return;
 	}
-	if (!isPowerOfTwo(ram)) {
-		ns.tprint("ERROR: ram (" + ram + ") must be a power of two.")
+	if (ramInput > 20 || ramInput < 1) {
+		ns.tprint(`ERROR: ram (${ram}) must be between 1 and 20.`)
 		return;
 	}
-	if (!ns.serverExists(target)) {
-		ns.tprint("ERROR: server " + target + " does not exist.")
-		return;
-	}
-	const scriptName = "/automation/util/grow-hack-weak.js"
-	const scriptRam = ns.getScriptRam(scriptName);
-	const threads = Math.floor(ram / scriptRam);
+
+
 
 	// Suffix for server names.
 	let i = ns.getPurchasedServers().length;
-
 	while (ns.getServerMoneyAvailable("home") > minMoneyThreshold && i < ns.getPurchasedServerLimit()) {
 		await ns.sleep(500);
 		// Check if we have enough money to purchase a server.  If we don't,
 		// this script will wait until we do.  This is useful in the early
 		// game when you don't have enough money to buy up to the limit.
 		if (ns.getServerMoneyAvailable("home") < ns.getPurchasedServerCost(ram)) {
+		//	ns.tprintf(`sleeping as server Money: ${ns.getServerMoneyAvailable("home")} < ${ns.getPurchasedServerCost(ram)}`)
 			continue;
 		}
 		const hostname = ns.purchaseServer("pserv-" + i, ram);
@@ -40,19 +35,9 @@ export async function main(ns) {
 			ns.tprint("ERROR: Failed to purchase server " + hostname)
 			return;
 		}
-		const files = [
-			scriptName,
-		]
-		await ns.scp(files, "home", hostname);
-		if (ns.exec(scriptName, hostname, threads, target) == 0) {
-			ns.tprint("ERROR: Launching " + scriptName + " failed on hostname " + hostname);
-			return;
-		}
+		ns.exec("/automation/commands/init-purchased-server.js", ns.getHostname(), 1, ...["--server", hostname])
+
 		i++;
 	}
 	ns.tprint("INFO: Congratulations, purchased server limit of " + i + " reached.")
-}
-
-function isPowerOfTwo(x) {
-	return (Math.log(x) / Math.log(2)) % 1 === 0
 }
