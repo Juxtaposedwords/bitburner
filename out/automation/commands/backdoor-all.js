902711677276eefae846afb1d8447207df4e40e7
juxtaposedwords <juxtaposedwords@gmail.com>
2022-02-05 14:15:08 -0800

    Adding value target sorting and implicit hacking

--- out/automation/commands/backdoor-all.js
+++ out/automation/commands/backdoor-all.js
@@ -15,14 +15,24 @@ export async function main(ns) {
     const purchased = ns.getPurchasedServers()
     let eligible = scan(ns).filter(function (name) {
         const server = ns.getServer(name);
-        return !server.backdoorInstalled && server.requiredHackingSkill <= ns.getHackingLevel() && server.hostname != 'home' && !purchased.includes(name)
+        safeRoot(ns, name)
+        return !server.backdoorInstalled &&
+            server.requiredHackingSkill <= ns.getHackingLevel() &&
+            server.hostname != 'home' && !purchased.includes(name) &&
+            server.hasAdminRights
     })
+    const valueTargets = ["avmnite-02h", "CSEC", "run4theh111z", "I.I.I.I"]
     eligible.sort((left, right) => { //sort by easiest to hardest
         const l = ns.getServer(left).requiredHackingSkill
         const r = ns.getServer(right).requiredHackingSkill
         if (l > r) return 1
         if (l < r) return -1
         return 0;
+    })
+    eligible.sort((left, right) => { //sort by notable targets (such as hacking functions)
+        if (valueTargets.includes(left) && valueTargets.includes(right)) { return 0 }
+        if (valueTargets.includes(left)) { return 1 }
+        if (valueTargets.includes(right)) { return -1 }
     })
     let source = ns.getHostname()
     await ns.tprintf("INFO: %s", eligible)
@@ -34,19 +44,7 @@ export async function main(ns) {
         let command = "connect " + hops.join("; connect ") + "; backdoor";
         await run(command);
         const serverLevel = await ns.getServer(dest).requiredHackingSkill
-        // If you have the formula API:
-        // await ns.formulas.hacking.hackTime(dest,ns.getPlayer());
-        if (serverLevel > 600) {
-            await ns.sleep(100000);
-        } else if (serverLevel > 500) {
-            await ns.sleep(150000);
-        } else if (serverLevel > 400) {
-            await ns.sleep(70000)
-        } else if (serverLevel > 150) {
-            await ns.sleep(30000)
-        } else {
-            await ns.sleep(10000);
-        }
+        await ns.sleep(ns.formulas.hacking.hackTime(dest, ns.getPlayer()));
         source = dest;
     }
 }
