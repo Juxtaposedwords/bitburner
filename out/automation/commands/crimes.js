fefa26ab71b466d75974d7e5ce3b7b133e1d272c
JuxtaposedWords <JuxtaposedWords@gmail.com>
2022-02-22 23:14:52 -0800

    Added percent success to crime list

--- out/automation/commands/crimes.js
+++ out/automation/commands/crimes.js
@@ -23,19 +23,20 @@ export async function main(ns) {
     ])
     if (flags.list) {
         ns.tprintf(`All stats are listed in xp - weighted success rate`)
-        let title = `${`Crime`.padEnd(21, " ")}    Money     Time(s)   Karma         Str`
-        title += ` ${`Def`.padStart(13, " ")}`
-        title += ` ${`Dex`.padStart(13, " ")}`
-        title += ` ${`Agi`.padStart(13, " ")}`
-        title += ` ${`Cha`.padStart(13, " ")}`
+        let title =`${`Crime`.padEnd(21," ")}    Money     Win %%  Time(s)   Karma         Str`
+        title += ` ${`Def`.padStart(13," ")}`
+        title += ` ${`Dex`.padStart(13," ")}`
+        title += ` ${`Agi`.padStart(13," ")}`
+        title += ` ${`Cha`.padStart(13," ")}`
 
         ns.tprintf(title)
         for (const crime of crimes) {
             const stats = ns.getCrimeStats(crime)
-            let output = `${crime.padEnd(23, " ")}  `
-            output += `${`${ns.nFormat(stats.money, "$0.0a")}`.padEnd(10, " ")}`
-            output += `${`${(stats.time / 1000)}`.padEnd(3, " ")}       `
-            output += `${`${-1 * stats.karma}`.padEnd(5, " ")}   `
+            let output = `${crime.padEnd(23," ")}  `
+            output +=`${`${ns.nFormat(stats.money, "$0.0a")}`.padEnd(10," ")}`
+            output += `${`${Math.floor(ns.getCrimeChance(crime)*100)}`.padEnd(5, " ")}  `
+            output += `${`${(stats.time/1000)}`.padEnd(3," ")}       `
+            output += `${`${-1*stats.karma}`.padEnd(5, " ")}   `
             output += `${`${stats.strength_exp}`.padStart(4, " ")} - ${`${stats.strength_success_weight}`.padStart(4, " ")}   `
             output += `${`${stats.defense_exp}`.padStart(4, " ")} - ${`${stats.defense_success_weight}`.padStart(4, " ")}   `
             output += `${`${stats.dexterity_exp}`.padStart(4, " ")} - ${`${stats.dexterity_success_weight}`.padStart(4, " ")}   `
@@ -50,18 +51,18 @@ export async function main(ns) {
         ns.tprintf(`ERROR: "${crime}" is not include in crimeTime list`)
         return
     }
-    if (ns.fileExists(lockFile)) {
-        ns.rm(lockFile, "home")
+    if (ns.fileExists(lockFile)){
+        ns.rm(lockFile,"home")
     }
     let started = Date.now()
     while (true) {
-        await ns.commitCrime(crime)
+                await ns.commitCrime(crime)
         started = Date.now()
-        while (ns.isBusy()) {
+        while (ns.isBusy()){
             await ns.sleep(100)
         }
         // Without this check, crimes will just your screen and you'll have to kill all scripts.
-        if ((Date.now() - started) < ns.getCrimeStats(crime).time - 300) {
+        if ((Date.now()-started) <ns.getCrimeStats(crime).time-300){
             return
         }
     }
